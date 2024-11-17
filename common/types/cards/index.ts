7b0203f2955073da0c010af4910193d12adb949c
Arturo Cruz <arturocrzc@gmail.com>
2024-11-17 17:12:26 -0600

    adding swagger schema to card options

--- common/types/cards/index.ts
+++ common/types/cards/index.ts
@@ -8,6 +8,37 @@ export enum CardTypes {
     SERVICES = 5 // e.g AMEX PLATINUM
 }
 
+/**
+* @swagger
+* components:
+*   schemas:
+*       CardOptions:
+*           type: object
+*           properties:
+*               cardNumber:
+*                   type: string
+*               holderName:
+*                   type: string
+*               expires:
+*                   type: string
+*                   format: date
+*               issuer:
+*                   type: object
+*                   $ref: "#/components/schemas/ContactInfo"
+*               balance:
+*                   type: number
+*               alias
+*                   type: string
+*               limit:
+*                   type: number
+*           required:
+*               - cardNumber
+*               - holderName
+*               - expires
+*               - issuer
+*               - balance
+*/
+/** Interface that serves as the payload for creating a new user card. */
 export interface CardOptions {
     cardNumber: string;
     holderName: string;
