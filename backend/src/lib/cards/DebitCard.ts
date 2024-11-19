46f79c57522704e963ef55244ea824270d415a32
Arturo Cruz <arturocrzc@gmail.com>
2024-11-18 18:53:58 -0600

    Updating set of isVoucher attrib after voucher enum was removed from CardTypes

--- backend/src/lib/cards/DebitCard.ts
+++ backend/src/lib/cards/DebitCard.ts
@@ -4,8 +4,7 @@ import { CardOptions, CardTypes } from "@common/types/cards";
 export class DebitCard extends Card {
     protected isVoucher: boolean;
     constructor(options: CardOptions, isVoucher: boolean) {
-        const type = isVoucher ? CardTypes.VOUCHER : CardTypes.DEBIT;
-        super(options, type);
+        super(options, CardTypes.DEBIT);
         this.isVoucher = isVoucher;
     }
 }
