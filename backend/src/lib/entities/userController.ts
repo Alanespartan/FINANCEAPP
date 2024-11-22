11779ee5497e433f04ab8a8fdb6bd9459424fbf3
Arturo Cruz <arturocrzc@gmail.com>
2024-11-22 10:51:59 -0600

    Creating dedicated files for user controller class

---
+++ backend/src/lib/entities/userController.ts
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import { Logger } from "@common/types/logger";
+import { randomUUID } from "crypto";
+import { User } from "./user";
+
+export class UserController {
+    // mimic a database storage
+    userStore: Record<string, User> = {};
+
+    public create(email: string, password: string, firstName: string, lastName: string) {
+        const id = randomUUID();
+        if(!this.userStore[id]) this.userStore[id] = new User(id, email, password, firstName, lastName);
+    }
+
+    public get(id: string) {
+        if(this.userStore[id]) return this.userStore[id];
+        return undefined;
+    }
+
+    public getByEmail(email: string) {
+        // dbConnection.query()
+
+        for(const key in this.userStore) {
+            const user = this.userStore[key];
+            if(email === user.email) return user;
+        }
+        return undefined;
+    }
+
+    public discard(id: string) {
+        return delete this.userStore[id];
+    }
+}
+
+export const userController = new UserController();
+userController.create("test@gmail.com", "admin", "John", "Doe");
