562cabb8851aa1640378168ca1a2f1df2a614cb3
Arturo Cruz <arturocrzc@gmail.com>
2024-11-22 13:51:05 -0600

    Updating export from cards index file after credit and debit cards classes were removed

--- backend/src/lib/cards/index.ts
+++ backend/src/lib/cards/index.ts
@@ -1,12 +1,1 @@
-import { Card } from "./Card";
-import { DebitCard } from "./DebitCard";
-import { CreditCard } from "./CreditCard";
-
-type AvailableCards = DebitCard | CreditCard;
-
-export {
-    Card,
-    DebitCard,
-    CreditCard,
-    AvailableCards
-};
+export * from "./Card";
