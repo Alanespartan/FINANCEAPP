fbb3dd48d68ac9a7680fdc9a33e0679be940399d
Arturo Cruz <arturocrzc@gmail.com>
2024-11-22 19:03:16 -0600

    Adding export of bank class

--- backend/src/lib/entities/index.ts
+++ backend/src/lib/entities/index.ts
@@ -1,9 +1,11 @@
 import { User } from "./users/user";
 import { Card } from "./cards/card";
 import { Loan } from "./loans/loan";
+import { Bank } from "./banks/bank";
 
 export {
     User,
     Card,
-    Loan
+    Loan,
+    Bank
 };
