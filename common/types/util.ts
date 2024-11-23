3c080779da824e6668a1af28ebd8c7eb6c4319f5
Arturo Cruz <arturocrzc@gmail.com>
2024-11-23 12:43:37 -0600

    Adding object enum for pay frequency options

--- common/types/util.ts
+++ common/types/util.ts
@@ -25,3 +25,37 @@ export interface IBank {
     name: string;
     country: string;
 }
+
+/**
+* @swagger
+* components:
+*   schemas:
+*       OEPayFrequency:
+*           type: integer
+*           description: Object Enum representing all the pay frenquency options available.
+*           enum: [0, 1, 2, 3]
+*           x-enum-varnames: [SemiWeekly, Weekly, SemiMonthly, Monthly]
+*           properties:
+*               0:
+*                   description: Twice a week
+*               1:
+*                   description: Every week
+*               2:
+*                   description: Twice a month
+*               3:
+*                   description: Every month
+*/
+/** Enum used to identify all the possible cards the application can manipulate. */
+/** Object Enum representing all the pay frenquency options available. */
+export const OEPayFrequency = {
+    /** Twice a week (Every wednesday and friday): 104 pay checks per year. */
+    SemiWeekly: 0,
+    /** Every week (Last friday): 52 pay checks per year. */
+    Weekly: 1,
+    /** Twice a month (1st n 15th or 15th n 30th of every month): 24 pay checks per year. */
+    SemiMonthly: 2,
+    /** Every month (Last working day): 12 pay checks per year. */
+    Monthly: 3
+} as const;
+
+
