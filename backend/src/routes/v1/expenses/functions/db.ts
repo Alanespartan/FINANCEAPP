d9164f5343bd35d4e9adb54c094421b81b7cbfd7
Arturo Cruz <arturocrzc@gmail.com>
2024-11-29 21:19:59 -0600

    Adding methods in expense type data mapper to apply db changes

--- backend/src/routes/v1/expenses/functions/db.ts
+++ backend/src/routes/v1/expenses/functions/db.ts
@@ -1,5 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { TExpenseType } from "@common/types/expenses";
 import { expenseTypeStore } from "@db";
+import { ExpenseType } from "@entities";
+
+export async function createExpenseType(toSave: ExpenseType) {
+    await expenseTypeStore.save(toSave);
+}
 
 export async function getUserExpenseTypes(userId: number) {
     return await expenseTypeStore.find({
@@ -9,4 +15,25 @@ export async function getUserExpenseTypes(userId: number) {
             }
         }
     });
+}
+
+export async function getUserExpenseTypesByType(userId: number, filter: TExpenseType) {
+    return await expenseTypeStore.find({
+        where: {
+            userId: userId,
+            type: filter
+        }
+    });
+}
+
+export async function getExpenseTypeById(expenseTypeId: number) {
+    return await expenseTypeStore.findOne({
+        where: {
+            id: expenseTypeId
+        }
+    });
+}
+
+export async function getByFilters(conditions: any) { // TODO EXPENSE TYPES create interface to define potencial non undefined properties to search for
+    return await expenseTypeStore.find({ where: conditions });
 }
