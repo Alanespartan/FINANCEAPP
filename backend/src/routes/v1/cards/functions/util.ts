4684011e7fa82e4eece5b52db3134ea369a8f509
Arturo Cruz <arturocrzc@gmail.com>
2024-11-30 16:41:27 -0600

    Moving into card util file the function to build payload from non null attributes in object

--- backend/src/routes/v1/cards/functions/util.ts
+++ backend/src/routes/v1/cards/functions/util.ts
@@ -1,4 +1,5 @@
-import { TCardFilters, TCardTypes, OECardTypesFilters }   from "@common/types/cards";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TCardFilters, TCardTypes, OECardTypesFilters, UpdateCardPayload }   from "@common/types/cards";
 import { Card } from "@entities";
 
 export const isValidCardFilter = (value: number): value is TCardFilters => {
@@ -27,4 +28,15 @@ export function MapNewToExistingArray(newCard: Card) {
         userId:     newCard.userId,
         bankId:     newCard.bankId,
     } as Card;
+}
+
+/** Helper function to remove null|undefined attributes from given card update options. */
+export function filterNonNullableAttributes(options: UpdateCardPayload) {
+    // Create a new object with only defined keys
+    return Object.entries(options).reduce((acc, [ key, value ]) => {
+        if(value !== undefined && value !== null) {
+            acc[key as keyof UpdateCardPayload] = value;
+        }
+        return acc;
+    }, {} as any);
 }
