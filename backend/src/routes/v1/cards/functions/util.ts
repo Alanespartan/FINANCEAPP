732afd76459cef3b54c83338c9a03552faeaa89f
Arturo Cruz <arturocrzc@gmail.com>
2024-11-30 16:44:23 -0600

    Removing non used function

--- backend/src/routes/v1/cards/functions/util.ts
+++ backend/src/routes/v1/cards/functions/util.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { TCardFilters, TCardTypes, OECardTypesFilters, UpdateCardPayload }   from "@common/types/cards";
-import { Card } from "@entities";
 
 export const isValidCardFilter = (value: number): value is TCardFilters => {
     return value === OECardTypesFilters.ALL
@@ -14,21 +13,6 @@ export const isValidCardType = (value: number): value is TCardTypes => {
         || value === OECardTypesFilters.CREDIT
         || value === OECardTypesFilters.SERVICES;
 };
-
-export function MapNewToExistingArray(newCard: Card) {
-    return {
-        cardNumber: newCard.getCardNumber(),
-        name:       newCard.getName(),
-        expires:    newCard.getExpirationDate(),
-        balance:    newCard.getBalance(),
-        type:       newCard.getCardType(),
-        archived:   newCard.getArchived(),
-        limit:      newCard.getCardType() === OECardTypesFilters.CREDIT ? newCard.getLimit() : null,
-        isVoucher:  newCard.getIsVoucher(),
-        userId:     newCard.userId,
-        bankId:     newCard.bankId,
-    } as Card;
-}
 
 /** Helper function to remove null|undefined attributes from given card update options. */
 export function filterNonNullableAttributes(options: UpdateCardPayload) {
