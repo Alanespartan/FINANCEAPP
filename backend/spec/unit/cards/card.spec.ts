b4ba7aac93074f50fa3adc7bbdd842a80ac4c465
Arturo Cruz <arturocrzc@gmail.com>
2024-12-01 18:53:30 -0600

    Adding more tests to card.spec and fix naming convention

--- backend/spec/unit/cards/card.spec.ts
+++ backend/spec/unit/cards/card.spec.ts
@@ -29,20 +29,68 @@ after(async () => {
     if(res.status !== 200) { throw new Error("Logout failed!"); }
 });
 
-
-describe("API Cards Tests", function() {
-    describe("Create Card", function() {
-        describe("Missing cardType header", function() {
-            it("Should throw 400 Bad Request Error", async function() {
+describe(`Testing API: ${cardPath}`, function() {
+    // #region Create Card Tests
+    describe("When Creating a Card", function() {
+        describe("Given invalid headers", function() {
+            it("Must throw a 400 Bad Request Error if 'cardType' header is missing", async function() {
                 const res = await agent
                     .post(cardPath)
                     .expect(400)
                     .expect("Content-Type", /json/);
-
                 expect(res.body).to.have.property("status", "error");
                 expect(res.body).to.have.property("message", "The server did not understand the request or could not read the request body.");
                 expect(res.body).to.have.property("info", "Expected 'cardType' header was not provided.");
             });
+            it("Must throw a 400 Bad Request Error if an invalid type is used", async function() {
+                const res = await agent
+                    .set("cardType", "-1")
+                    .post(cardPath)
+                    .expect(400)
+                    .expect("Content-Type", /json/);
+                expect(res.body).to.have.property("status", "error");
+                expect(res.body).to.have.property("message", "The server did not understand the request or could not read the request body.");
+                expect(res.body).to.have.property("info", "Invalid type for creating a new card.");
+            });
+        });
+        describe("Given invalid payload", function() {
+            const invalidCardNumber = {
+                cardNumber: "482069a7378b92153z1",
+                expires:    new Date(),
+                bankId:     1,
+                balance:    10000,
+                name:       "Visa Débito BBVA Nómina",
+                limit:      10000,
+                isVoucher:  false
+            };
+            it("Must throw a 500 Server Error if a malformed payload is used", async function() {
+                const res = await agent
+                    .set("cardType", "1")
+                    .post(cardPath)
+                    .send({
+                        incorrectProp1: "",
+                        nonExistingProp: 2
+                    })
+                    .expect(500)
+                    .expect("Content-Type", /json/);
+                expect(res.body).to.have.property("status", "error");
+                expect(res.body).to.have.property("message", "A typescript error occurred while trying to process your request.");
+            });
+            it("Must throw a 400 Bad Request Error if a card number contains non numeric chars", async function() {
+                const res = await agent
+                    .set("cardType", "1")
+                    .post(cardPath)
+                    .send(invalidCardNumber)
+                    .expect(400)
+                    .expect("Content-Type", /json/);
+                expect(res.body).to.have.property("status", "error");
+                expect(res.body).to.have.property("message", "The server did not understand the request or could not read the request body.");
+                expect(res.body).to.have.property("info", "Invalid card number \"482069a7378b92153z1\". A card number can not contain non numeric chars.");
+            });
         });
     });
+    // #endregion Create Card Tests
+
+    // #region Fetch Card Tests
+    // #endregion Fetch Card Tests
 });
