0a35391a817afc2de6b8140f4b4a628e74ffbe51
Arturo Cruz <arturocrzc@gmail.com>
2024-12-05 15:29:56 -0600

    Adding helper method for checking expense type exists in user info

--- backend/src/lib/entities/users/methods/expenseTypes.ts
+++ backend/src/lib/entities/users/methods/expenseTypes.ts
@@ -1,24 +1,17 @@
 import { User, ExpenseType } from "@entities";
 
-/**
-* Save a new expense category in user information.
-* @param {ExpenseType} toAdd Contains information of new expense category.
-*/
 export function addExpenseType(this: User, toAdd: ExpenseType) {
     this.expenseTypes.push(toAdd);
 }
 
-/**
-* Helper function that obtains the desired expense type.
-* @param {string} name Expense type name to search for.
-*/
+export function hasExpenseType(this: User, toSearch: string): boolean {
+    return this.expenseTypes.find((et) => et.name === toSearch) ? true : false;
+}
+
 export function getExpenseType(this: User, name: string) {
     return this.expenseTypes.find((ec) => ec.name === name);
 }
 
-/**
-* Get all stored user expense types.
-*/
 export function getExpenseTypes(this: User) {
     return this.expenseTypes;
 }
