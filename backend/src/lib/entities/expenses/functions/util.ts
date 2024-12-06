fc2b371afd53feafb6f265566e4cd1eeebbcde33
Arturo Cruz <arturocrzc@gmail.com>
2024-12-06 16:08:44 -0600

    Updating util expenses functions since savings were removed and to handle 2 payload types

--- backend/src/lib/entities/expenses/functions/util.ts
+++ backend/src/lib/entities/expenses/functions/util.ts
@@ -1,27 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { OETypesOfExpense, TExpenseType, TExpenseTypeFilter, UpdateExpenseTypePayload } from "@common/types/expenses";
+import { OETypesOfExpense, TExpenseType, TExpenseTypeFilter, UpdateExpenseCategoryPayload, UpdateExpenseSubCategoryPayload } from "@common/types/expenses";
 
 export const isValidExpenseType = (value: number): value is TExpenseType => {
     return value === OETypesOfExpense.REALEXPENSE
         || value === OETypesOfExpense.CARD
-        || value === OETypesOfExpense.LOAN
-        || value === OETypesOfExpense.SAVINGS;
+        || value === OETypesOfExpense.LOAN;
 };
 
 export const isValidExpenseTypeFilter = (value: number): value is TExpenseTypeFilter => {
     return value === OETypesOfExpense.ALL
         || value === OETypesOfExpense.REALEXPENSE
         || value === OETypesOfExpense.CARD
-        || value === OETypesOfExpense.LOAN
-        || value === OETypesOfExpense.SAVINGS;
+        || value === OETypesOfExpense.LOAN;
 };
 
 /** Helper function to remove null|undefined attributes from given expense type update options. */
-export function filterNonNullableAttributes(options: UpdateExpenseTypePayload) {
+export function filterNonNullableAttributes(options: UpdateExpenseCategoryPayload | UpdateExpenseSubCategoryPayload) {
     // Create a new object with only defined keys
     return Object.entries(options).reduce((acc, [ key, value ]) => {
         if(value !== undefined && value !== null) {
-            acc[key as keyof UpdateExpenseTypePayload] = value;
+            acc[key] = value;
         }
         return acc;
     }, {} as any);
