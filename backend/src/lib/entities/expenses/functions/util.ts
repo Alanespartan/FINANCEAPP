c684e045a2ff0f71e4d5e6901d4157691043e1b0
Arturo Cruz <arturocrzc@gmail.com>
2024-12-09 18:57:33 -0600

    Adding regions to categories router and add more safety checks for create category endpoint

--- backend/src/lib/entities/expenses/functions/util.ts
+++ backend/src/lib/entities/expenses/functions/util.ts
@@ -1,5 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { OETypesOfExpense, TExpenseType, TExpenseTypeFilter, UpdateExpenseCategoryPayload, UpdateExpenseSubCategoryPayload } from "@common/types/expenses";
+import { OETypesOfExpense, TExpenseType, TExpenseTypeFilter,
+    UpdateExpenseCategoryPayload, UpdateExpenseSubCategoryPayload,
+    CreateExpenseCategoryPayload
+} from "@common/types/expenses";
+
+/**
+ * Ensures create expense category body is in correct format and all required information is present.
+ * @param {unknown} body Body from create expense category request.
+ * @returns bodyisCreateExpenseCategoryPayload wheter or not the body is valid
+ */
+export function verifyCreateExpenseCategoryBody(body: unknown): body is CreateExpenseCategoryPayload {
+    if(typeof body !== "object") return false;
+    if(!body) return false;
+    const parsed = body as CreateExpenseCategoryPayload;
+
+    return typeof parsed.name === "string" && typeof parsed.isDefault === "boolean";
+}
 
 export const isValidExpenseSubCategory = (value: number): value is TExpenseType => {
     return value === OETypesOfExpense.REALEXPENSE
@@ -18,7 +34,7 @@ export const isValidExpenseSubCategoryFilter = (value: number): value is TExpens
         || value === OETypesOfExpense.LOAN;
 };
 
-/** Helper function to remove null|undefined attributes from given expense type update options. */
+/** Helper function to remove null|undefined attributes from given expense category/subcategory update options. */
 export function filterNonNullableAttributes(options: UpdateExpenseCategoryPayload | UpdateExpenseSubCategoryPayload) {
     // Create a new object with only defined keys
     return Object.entries(options).reduce((acc, [ key, value ]) => {
