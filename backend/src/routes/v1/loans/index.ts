422f3ca49ecd68f55b4d459e2268d3da3d4ba9b0
Arturo Cruz <arturocrzc@gmail.com>
2024-12-12 19:59:39 -0600

    Adding endpoint to get all loans a user has registered

--- backend/src/routes/v1/loans/index.ts
+++ backend/src/routes/v1/loans/index.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import { BadRequestError } from "@errors";
-import { ConvertToUTCTimestamp } from "@backend/utils/functions";
-import { Loan, ExpenseSubCategory } from "@entities";
-import { verifyCreateLoanBody } from "@entities/loans/functions/util";
-import { getBank } from "@entities/cards/functions/db";
+import { ConvertToUTCTimestamp, isValidPayFrequency } from "@backend/utils/functions";
+import { validateQueryParams } from "@backend/utils/requests";
+import {
+    saveExpenseCategory,
+    saveExpenseSubCategory
+} from "@entities/expenses/functions/db";
+import {
+    CreateExpenseSubCategoryPayload,
+    OETypesOfExpense
+} from "@common/types/expenses";
+import { getBank  } from "@entities/cards/functions/db";
 import { saveLoan } from "@entities/loans/functions/db";
-import { saveExpenseCategory, saveExpenseSubCategory } from "@entities/expenses/functions/db";
-import { CreateExpenseSubCategoryPayload, OETypesOfExpense } from "@common/types/expenses";
+import { verifyCreateLoanBody }     from "@entities/loans/functions/util";
+import { Loan, ExpenseSubCategory } from "@entities";
+import { BadRequestError } from "@errors";
 
 const router = Router();
 
@@ -53,7 +60,7 @@ router.post("/", async (req, res, next) => {
             throw new BadRequestError(`Loan "${options.name}" cannot be created because one with that name already exists.`);
         }
 
-        /* LOAN EXPIRATION DATE */
+        // validate loan expiration date is correct
         if(options.expires) {
             // using UTC function for correct timestamp comparision
             if(ConvertToUTCTimestamp(options.expires) < ConvertToUTCTimestamp(new Date())) {
@@ -61,15 +68,14 @@ router.post("/", async (req, res, next) => {
             }
         }
 
-        const newLoan = new Loan(options, user.id);
-
         // save new loan in db
+        const newLoan = new Loan(options, user.id);
         const savedLoan = await saveLoan(newLoan);
 
         // get loans default parent expense category
         let loansCategory = user.getExpenseCategoryByName("Loans");
 
-        // create new expense sub category using loan info so we can register when paying "TO THIS CARD"
+        // create new expense sub category using loan info so we can register when paying "TO THIS LOAN"
         const toSaveSubCategory = new ExpenseSubCategory(
             {
                 name: savedLoan.name,
@@ -77,10 +83,10 @@ router.post("/", async (req, res, next) => {
                 instrumentId: savedLoan.id
             } as CreateExpenseSubCategoryPayload, user.id
         );
-        // save updated loans category which is the owner of the relationship (it has JoinColumn decorator and cascade) and has the new sub category
+        // save new loan sub category
         const savedLoansSubCategory = await saveExpenseSubCategory(toSaveSubCategory);
 
-        // add sub category into "Loans" category
+        // add saved loan sub category into "Loans" category
         loansCategory.addSubCategory(savedLoansSubCategory);
 
         // add relationship between loans category and new sub category into many to many table
@@ -95,5 +101,70 @@ router.post("/", async (req, res, next) => {
     } catch(error) { return next(error); }
 });
 // #endregion POST Loan
+
+// #region GET Loans
+/**
+* @swagger
+* /api/v1/loans:
+*   get:
+*       summary: Get user loans
+*       description: Get all loans a user has registered.
+*       tags:
+*           - Loans
+*       parameters:
+*           - in: query
+*             name: archived
+*             schema:
+*               type: boolean
+*           - in: query
+*             name: isFinished
+*             schema:
+*               type: boolean
+*           - in: query
+*             name: payFrequency
+*             schema:
+*               type: integer
+*       responses:
+*           200:
+*               description: An array of loans a user has registered.
+*               content:
+*                   application/json:
+*                       schema:
+*                           type: array
+*                           items:
+*                               $ref: "#/components/schemas/ILoan"
+*           400:
+*               description: Bad Request Error
+*/
+router.get("/", async (req, res, next) => {
+    try {
+        const user     = req.userData;
+        // optional filters
+        const { archived, isFinished, payFrequency } = req.query;
+
+        // Validate query parameters
+        const archivedFilter     = validateQueryParams(archived,     "boolean", "archived");
+        const isFinishedFilter   = validateQueryParams(isFinished,   "boolean", "isFinished");
+        const payFrequencyFilter = validateQueryParams(payFrequency, "number",  "payFrequency");
+
+        // Apply filters
+        let loans = user.getLoans();
+        if(archivedFilter !== undefined) {
+            loans = loans.filter((loan) => loan.archived === archivedFilter);
+        }
+        if(isFinishedFilter !== undefined) {
+            loans = loans.filter((loan) => loan.isFinished === isFinishedFilter);
+        }
+        if(payFrequencyFilter !== undefined) {
+            if(payFrequencyFilter && !isValidPayFrequency(payFrequencyFilter as number)) {
+                throw new BadRequestError("Invalid payFrequency value.");
+            }
+            loans = loans.filter((loan) => loan.payFrequency === payFrequencyFilter);
+        }
+
+        return res.status(200).json(loans);
+    } catch(error) { return next(error); }
+});
+// #endregion GET Loans
 
 export default router;
